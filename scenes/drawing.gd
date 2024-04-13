efaf3b24a057c657b462e3905ed6301119c38096
budermich <budermich5@gmail.com>
2024-04-13 19:10:06 +0300

    added art drawing

--- scenes/drawing.gd
+++ scenes/drawing.gd
@@ -1,6 +1,13 @@
 extends Node2D
+var CirclePos: Array = []
 
-
+func _input(event: InputEvent)->void:
+	if  !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		return
+		
+	CirclePos.append(event.position)
+	queue_redraw()
 func _draw() ->void:
-	draw_circle(Vector2(100,100),10, Color.RED)
+		for point in CirclePos:
+			draw_circle(point,10, Color.RED)
 	
