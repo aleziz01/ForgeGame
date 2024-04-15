a584d38e4a2aff7d9cf9baa6a68ac3cdb3e36ff3
aleziz01 <alex.artene@gmail.com>
2024-04-15 17:28:46 +0600

    added pixel art
    and a canvas

--- scenes/drawing.gd
+++ scenes/drawing.gd
@@ -5,21 +5,42 @@ var simultaneous_scene = preload("res://scenes/MainWorld.tscn")
 func _on_exit_button_pressed():
 	get_tree().change_scene_to_file ("res://scenes/MainWorld.tscn")
 	global.drawing=false
-
-
+var color=Color(0, 0, 0)
 
 func _input(event: InputEvent):
 	if  !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		pass
+		return
 	else:
+		
 		CirclePos.append(event.position)
 		queue_redraw()
 
-
+func _on_yellow_pressed():
+	color=Color(1,1,0)
+func _on_black_pressed():
+	color=Color(0,0,0)
+func _on_green_pressed():
+	color=Color(0,1,0)
+func _on_blue_pressed():
+	color=Color(0,0,1)
+func _on_red_pressed():
+	color=Color(1,0,0)
+func _on_white_pressed():
+	color=Color(1,1,1)
 
 func _draw():
 		for point in CirclePos:
 			#draw_circle(point,10, Color.RED)
-			var rect_pos = point - Vector2(10, 10)  # Adjust position to define top-left corner
-			var rect_size = Vector2(20, 20)  # Define rectangle size
-			draw_rect(Rect2(rect_pos, rect_size), Color(1, 0, 0))  # Draw rectangle
+			var rect_pos = point  # Adjust position to define top-left corner
+			var rect_size = Vector2(8, 8)  # Define rectangle size
+			rect_pos.x=(int(rect_pos.x)/8)*8
+			rect_pos.y=(int(rect_pos.y)/8)*8
+			draw_rect(Rect2(rect_pos, rect_size), color)  # Draw rectangle
+			
+
+
+
+
+
+
+
