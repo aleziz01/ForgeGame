d5bcd52fa166d476b7973263dee4fce5584989ed
budermich <budermich5@gmail.com>
2024-04-19 23:50:32 +0300

    ALEX FIX THE ANIMATION PROBLEM BEFORE SUNUP

--- scenes/MainWorld.gd
+++ scenes/MainWorld.gd
@@ -11,16 +11,43 @@ func _on_exit_pressed():
 
 #spawning guys
 func _ready():
+	$GoPaint.hide()
+	var anim = $Smith
+	anim.play("idle")
 	await get_tree().create_timer(1.5).timeout
 	global.spawn = true
 	var customer = customer1.instantiate()
-	customer.position = Vector2(1000,500)
+	customer.position = Vector2(1300,500)
 	add_child(customer)
+	
+	
+func _process(delta):
+	if global.idle == true:
+		$GoPaint.show()
 
 func _on_go_paint_pressed():
 	if get_overlapping_bodies().size()>0 and global.drawing==false and global.idle == true:
 		get_tree().change_scene_to_file ("res://scenes/drawing.tscn")
-		$Floor.hide()
+		$Nfloor.hide()
+		$Nfloor2.hide()
+		$Nfloor3.hide()
+		$Nfloor4.hide()
+		$Nfloor5.hide()
+		$Nfloor6.hide()
+		$Nfloor7.hide()
+		$Nfloor8.hide()
+		$Nfloor9.hide()
+		$Nfloor10.hide()
+		$Nfloor11.hide()
+		$Nfloor12.hide()
+		$Nfloor13.hide()
+		$Nfloor14.hide()
+		$Nfloor15.hide()
+		$Nfloor16.hide()
+		$Nfloor17.hide()
+		$Nfloor18.hide()
+		$Nfloor19.hide()
+		$Nfloor20.hide()
 		$Smith.hide()
 		$GoPaint.hide()
 		global.drawing=true
